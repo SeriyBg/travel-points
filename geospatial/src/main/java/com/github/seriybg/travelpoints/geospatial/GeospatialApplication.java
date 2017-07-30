2f24cd5d456c2bc937431327c91b3e9ae713bf2c
Serge Bishyr <seriybg@gmail.com>
2017-07-30 13:00:59 +0300

    Add usage of testcontainer

--- geospatial/src/main/java/com/github/seriybg/travelpoints/geospatial/GeospatialApplication.java
+++ geospatial/src/main/java/com/github/seriybg/travelpoints/geospatial/GeospatialApplication.java
@@ -2,8 +2,10 @@ package com.github.seriybg.travelpoints.geospatial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class GeospatialApplication {
 
 	public static void main(String[] args) {
--- geospatial/src/test/java/com/github/seriybg/travelpoints/geospatial/GeospatialApplicationTests.java
+++ geospatial/src/test/java/com/github/seriybg/travelpoints/geospatial/GeospatialApplicationTests.java
@@ -1,13 +1,36 @@
 package com.github.seriybg.travelpoints.geospatial;
 
+import org.jetbrains.annotations.NotNull;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.GenericContainer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(initializers = GeospatialApplicationTests.Initializer.class)
 public class GeospatialApplicationTests {
+
+	@ClassRule
+	public static GenericContainer mongodb =
+			new GenericContainer("mongo:latest")
+					.withExposedPorts(27017);
+
+	public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		@Override
+		public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
+			TestPropertyValues.of(
+					"spring.data.mongodb.host=" + mongodb.getContainerIpAddress(),
+					"spring.data.mongodb.port=" + mongodb.getMappedPort(27017))
+			.applyTo(configurableApplicationContext);
+		}
+	}
 
 	@Test
 	public void contextLoads() {

d6696da50438a8c7750e231821528c95ee111a38
Serge Bishyr <seriybg@gmail.com>
2017-07-31 12:12:42 +0300

    implement GeoJson

--- geospatial/src/main/java/com/github/seriybg/travelpoints/geospatial/TravelLocation.java
+++ geospatial/src/main/java/com/github/seriybg/travelpoints/geospatial/TravelLocation.java
@@ -3,14 +3,19 @@ package com.github.seriybg.travelpoints.geospatial;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.geo.Point;
+import org.springframework.data.mongodb.core.geo.GeoJson;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Document
 @AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode()
 @ToString
-public class TravelLocation {
+public class TravelLocation implements GeoJson<List<Double>> {
+
+    private static final String TYPE = "Feature";
 
     @Id
     @Getter
@@ -22,7 +27,15 @@ public class TravelLocation {
     @Getter
     private String description;
 
-    @Getter
     private Point location;
 
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+
+    @Override
+    public List<Double> getCoordinates() {
+        return Arrays.asList(location.getX(), location.getY());
+    }
 }
